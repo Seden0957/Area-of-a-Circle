c5e5016c134c1b0e2cee42e440576ef620c093d2
seden0957 <seden0957@7KXL6Y1.Student.UGDSB.ED>
2018-03-08 12:58:28 -0500

    9head

---
+++ src/area/of/a/circle/AreaOfACircle.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package area.of.a.circle;
+
+/**
+ *
+ * @author seden0957
+ */
+public class AreaOfACircle {
+
+    
+    public static void main(String[] args) {
+        double radius, pi, area;
+        radius = 15; 
+        pi = 3.14159265359;
+        area = radius*pi*radius;
+        System.out.println("Area = " + area);
+    }
+    
+}
